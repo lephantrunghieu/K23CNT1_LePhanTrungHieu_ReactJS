48abee8c34e710204237d6ed8150bfaaaf890eba
unknown <lephantrunghieu555@gmail.com>
2025-02-10 15:06:07 +0700

    chinh sua

---
+++ Day01/const-demo.js
@@ -0,0 +1,5 @@
+const PI = 3.14;
+console.log(PI); 
+
+PI=3.1415;
+console.log(PI);
--- Day01/let-demo.js
+++ Day01/let-demo.js
@@ -1,9 +1,18 @@
-//let 
-let x=100;
-if(x===100){
-    let x=200;
-    console.log("x",x);
+//var
+var x=5;
+if(x===5){
+    var x=10;
+    console.log("x ben trong if",x);
 }
 //x ngoai
 console.log("x ben ngoai if: ",x)
 
+//let
+
+let y=5;
+if(y===5){
+    let y=10;
+    console.log("x ben trong if:",y);
+}
+
+console.log("y ben ngoai if",y);
