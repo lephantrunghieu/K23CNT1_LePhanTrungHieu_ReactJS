39ca5550dcb06eeb818fe273f5a034c33e9f0a24
unknown <lephantrunghieu555@gmail.com>
2025-02-11 09:02:05 +0700

    Day02

---
+++ Day02/lpth-hello/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lê Phan Trung Hiếu <br />
+          0866569267 <br />
+          lephantrunghieu555@gmail.com <br />
+          K23CNT1 <br />
+          Hai năm đi Mỹ em chờ <br />
+          Hai năm đi lính em chờ anh không <br />
+          Hai năm anh nhập ngũ em có chờ anh không <br />
+          Hai năm anh phải sống với nỗi cô đơn từng đêm <br />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
